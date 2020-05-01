6bfc1c5090c6d40b7444dfe7490c505a61175ada
ims <ims21@users.sourceforge.net>
2020-05-01 10:07:54 +0200

    remove whitespaces

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setup_translate
 
 pkg = 'Extensions.MeteoViewer'
 setup (name = 'enigma2-plugin-extensions-meteoviewer',
-       version = '1.76',
-       description = 'meteo pictures viewer',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['*.png', '*.xml', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass = setup_translate.cmdclass, # for translation
-      )
+	version = '1.76',
+	description = 'meteo pictures viewer',
+	packages = [pkg],
+	package_dir = {pkg: 'plugin'},
+	package_data = {pkg: ['*.png', '*.xml', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
+	cmdclass = setup_translate.cmdclass, # for translation
+	)
